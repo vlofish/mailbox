dad69bde3f966de01284dfba621ae6e244e81802
vmijael <1945.github.m0dtb@slmail.me>
2022-08-21 20:22:18 -0500

    Adding Redux Selector to `NavbarFeat`

--- meda-mailbox/src/app/features/NavbarFeat.tsx
+++ meda-mailbox/src/app/features/NavbarFeat.tsx
@@ -1,10 +1,18 @@
+import { useSelector } from "react-redux";
 import { ButtonComp } from "../components/ButtonComp";
+import { MailboxInterface } from "../common/interfaces/";
+
+let total: number;
+let unread: number;
 
 export function NavbarFeat() {
+	total = useSelector((state: MailboxInterface) => state.total);
+	unread = useSelector((state: MailboxInterface) => state.unread);
+
 	return (
 		<div>
 			<p> Hi V. </p>
-			<p> {`You have X unread messages out of Y`} </p>
+			<p> {`You have ${ unread } unread messages out of ${ total } messages.`} </p>
 			<ButtonComp text="View Messages" />
 		</div>
 	);
