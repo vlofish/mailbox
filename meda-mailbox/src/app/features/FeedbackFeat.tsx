2300d2c3b213e3a79064d2886d2c99a1eddbc9f5
vmijael <1945.github.m0dtb@slmail.me>
2022-08-23 01:12:34 -0500

    `FeedbackFeat` Alpha Implementation

--- meda-mailbox/src/app/features/FeedbackFeat.tsx
+++ meda-mailbox/src/app/features/FeedbackFeat.tsx
@@ -1,36 +1,67 @@
-
+// =================================================================
 import {
   ref,
   onValue,
   child, push, update
 } from "firebase/database";
-import { db as FIRE_DB } from "../app.config";
+import { db as FIRE_DB, dbName as DB_NAME } from "../app.config";
+import { ButtonComp } from "../components/ButtonComp";
+import { TextareaComp } from "../components/TextareaComp";
+import { CheckboxGroupComp } from "../components/CheckboxGroupComp";
+// =================================================================
 
-// TODO: this will show all the previous comments people have left
-const starCountRef = ref(FIRE_DB, 'feedback/');
-onValue(starCountRef, (snapshot) => {
+let comment = '';
+const comments = ref(FIRE_DB, `${DB_NAME}/`);
+
+onValue(comments, (snapshot) => {
+  let counter = 1;
   const data = snapshot.val();
-  console.table(data);
+  if (data) {
+    for (let key in data) {
+      console.log(`========== Comment ${counter} ==========`);
+      console.log(`Message: ${data[key].message}`);
+      console.log(`Feeling: ${data[key].feeling}`);
+      console.log(`========== ========== ========== \n\n`);
+      counter++;
+    }
+  }
 });
+
+// TODO: protect with debounce from too many events
+//TODO: WIP
+const handleTextareaChange = (character: string) => {
+  console.log(character);
+  comment += character;
+  console.log(comment);
+}
 
 //POST
 function postComment() {
   const postData = {
-    message: "so so",
-    feeling: "ok",
+    message: comment,
+    feeling: "ok", // TBD
   };
   const updates: any = {};
-  const newPostKey = push(child(ref(FIRE_DB), 'feedback')).key;
+  const newPostKey = push(child(ref(FIRE_DB), DB_NAME)).key;
 
-  updates['/feedback/' + newPostKey] = postData;
+  updates[`/${DB_NAME}/` + newPostKey] = postData;
 
   return update(ref(FIRE_DB), updates);
 }
 
 export function FeedbackFeat() {
   return (
-    <button onClick={() => { postComment() }}>
-      Write dummy comment
-    </button>
+    <div>
+      <CheckboxGroupComp />
+      <TextareaComp 
+        title="Tell us why"
+        handleChange={(e: any) => { handleTextareaChange(e.target.value) }}
+        />
+      <br />
+      <br />
+      <ButtonComp
+        text="Post Comment"
+        handleClick={() => { postComment() }} />
+    </div>
   );
 }
