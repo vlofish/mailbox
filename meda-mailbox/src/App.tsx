90943d006b99fb54cc5dd68505a2fea54efd3bee
vmijael <1945.github.m0dtb@slmail.me>
2022-08-23 00:07:34 -0500

    Cleaning `App.tsx` from Firebase POC

--- meda-mailbox/src/App.tsx
+++ meda-mailbox/src/App.tsx
@@ -1,53 +1,9 @@
-import { initializeApp } from "firebase/app";
-import {
-  getDatabase,
-  ref,
-  onValue,
-  child, push, update
-} from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCyCenkclS_2VjoUCrlKNRJCkRiuGDpiNw",
-  authDomain: "meda-mailbox.firebaseapp.com",
-  projectId: "meda-mailbox",
-  storageBucket: "meda-mailbox.appspot.com",
-  messagingSenderId: "306578451479",
-  appId: "1:306578451479:web:6c8d6844d597b467684052",
-  measurementId: "G-M067RD9CLD",
-  databaseURL: "https://meda-mailbox-default-rtdb.firebaseio.com",// Can I cypher this?
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-// TODO: this will show all the previous comments people have left
-const starCountRef = ref(db, 'feedback/');
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  console.table(data);
-});
-
-//POST
-function postComment() {
-  const postData = {
-    message: "so so",
-    feeling: "ok",
-  };
-  const updates: any = {};
-  const newPostKey = push(child(ref(db), 'feedback')).key;
-
-  updates['/feedback/' + newPostKey] = postData;
-
-  return update(ref(db), updates);
-}
-
+import { FeedbackFeat } from "./app/features/FeedbackFeat";
 
 function App() {
   return (
     <div className="App">
-      <button onClick={() => { postComment() }}> 
-        Write dummy comment
-      </button>
+      <FeedbackFeat />
     </div>
   );
 }
