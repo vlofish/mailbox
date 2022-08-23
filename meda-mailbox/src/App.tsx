d7b83f44deb389c4e2e1c5f79d7543c8ccf7008a
vmijael <1945.github.m0dtb@slmail.me>
2022-08-22 23:52:53 -0500

    Firebase POC

--- meda-mailbox/src/App.tsx
+++ meda-mailbox/src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { initializeApp } from "firebase/app";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  child, push, update
+} from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyCenkclS_2VjoUCrlKNRJCkRiuGDpiNw",
+  authDomain: "meda-mailbox.firebaseapp.com",
+  projectId: "meda-mailbox",
+  storageBucket: "meda-mailbox.appspot.com",
+  messagingSenderId: "306578451479",
+  appId: "1:306578451479:web:6c8d6844d597b467684052",
+  measurementId: "G-M067RD9CLD",
+  databaseURL: "https://meda-mailbox-default-rtdb.firebaseio.com",// Can I cypher this?
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// TODO: this will show all the previous comments people have left
+const starCountRef = ref(db, 'feedback/');
+onValue(starCountRef, (snapshot) => {
+  const data = snapshot.val();
+  console.table(data);
+});
+
+//POST
+function postComment() {
+  const postData = {
+    message: "so so",
+    feeling: "ok",
+  };
+  const updates: any = {};
+  const newPostKey = push(child(ref(db), 'feedback')).key;
+
+  updates['/feedback/' + newPostKey] = postData;
+
+  return update(ref(db), updates);
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => { postComment() }}> 
+        Write dummy comment
+      </button>
     </div>
   );
 }
