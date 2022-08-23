88fcaa186cc1f0769c3c470e84af658fb094b864
vmijael <1945.github.m0dtb@slmail.me>
2022-08-23 00:06:41 -0500

    Adding Firebase Config to `app.config` File

---
+++ meda-mailbox/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCyCenkclS_2VjoUCrlKNRJCkRiuGDpiNw",
+  authDomain: "meda-mailbox.firebaseapp.com",
+  projectId: "meda-mailbox",
+  storageBucket: "meda-mailbox.appspot.com",
+  messagingSenderId: "306578451479",
+  appId: "1:306578451479:web:6c8d6844d597b467684052",
+  measurementId: "G-M067RD9CLD",
+  databaseURL: "https://meda-mailbox-default-rtdb.firebaseio.com",// Can I cypher this?
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+
+
