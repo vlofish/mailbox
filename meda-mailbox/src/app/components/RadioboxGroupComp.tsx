71f2bc9faf2d4d1f1a7eaf04a3634d63152d4f26
vmijael <1945.github.m0dtb@slmail.me>
2022-08-23 14:42:03 -0500

    Renaming Comp to `RadioboxGroup`

---
+++ meda-mailbox/src/app/components/RadioboxGroupComp.tsx
@@ -0,0 +1,17 @@
+export function RadioboxGroupComp(props: any) {
+  return(
+    <div onChange={ props.handleChange }>
+      <label>Tell us how you felt using our mailbox?</label>
+      <div>
+        <input type="radio" id="dissapointed" name="mailbox_feeling" value="dissapointed" />
+        <label> Dissapointed </label>
+
+        <input type="radio" id="ok" name="mailbox_feeling" value="ok" />
+        <label> Ok </label>
+
+        <input type="radio" id="happy" name="mailbox_feeling" value="happy" />
+        <label> Happy </label>
+      </div>
+    </div>
+  );
+}
