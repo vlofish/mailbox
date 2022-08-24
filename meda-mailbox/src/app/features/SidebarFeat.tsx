9f737ba7ab2e6607c5b8808c66da10465af23d2d
vmijael <1945.github.m0dtb@slmail.me>
2022-08-23 23:24:40 -0500

    PR#2 [Adding `DataGrid` to `SidebarFeat`]

--- meda-mailbox/src/app/features/SidebarFeat.tsx
+++ meda-mailbox/src/app/features/SidebarFeat.tsx
@@ -1,29 +1,22 @@
-const mockedMails: {subject: string, preview: string}[] = [
-	{
-		subject: 'S1',
-		preview: 'Hello S1'
-	},
-	{
-		subject: 'S2',
-		preview: 'Hello S2'
-	}
-]
+// ===================================================================
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+// ===================================================================
 
-export function SidebarFeat() {
+const rows: GridRowsProp = [
+	{ id: 1, col1: 'Hello', col2: 'World' },
+	{ id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+	{ id: 3, col1: 'MUI', col2: 'is Amazing' },
+];
+
+const columns: GridColDef[] = [
+	{ field: 'col1', headerName: 'Column 1', width: 150 },
+	{ field: 'col2', headerName: 'Column 2', width: 150 },
+];
+
+export default function SidebarFeat() {
 	return (
-		<div>	
-				{ mockedMails.map((mail, index) => {
-					return (
-						<div key={index}> 
-						<hr />
-						Subject: {mail.subject} 
-						<br /> 
-						Preview: {mail.preview}
-						<hr />
-						</div>
-					);
-				}) }
-			
+		<div style={{ height: 300, width: '100%' }}>
+			<DataGrid rows={rows} columns={columns} />
 		</div>
 	);
 }
