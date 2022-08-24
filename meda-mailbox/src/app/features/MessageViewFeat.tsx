b95e47de8370f902c18b19eb9fd858887b5b0487
vmijael <1945.github.m0dtb@slmail.me>
2022-08-23 23:13:31 -0500

    PR#2 [Adding `MuiButtonComp`]

--- meda-mailbox/src/app/features/MessageViewFeat.tsx
+++ meda-mailbox/src/app/features/MessageViewFeat.tsx
@@ -1,10 +1,20 @@
 import { ButtonComp } from "../components/ButtonComp";
+import { MUIButtonCompInterface } from "../common/interfaces/component.interface";
+
+const muiButtonProps: MUIButtonCompInterface = {
+	size: 'small',
+	color: 'error',
+	variant: 'outlined',
+}
 
 export function MessageViewFeat() {
 	return (
 		<div>
 			<div>
-				<ButtonComp text="X" />
+				<ButtonComp 
+					text="X"
+					mui={ muiButtonProps }
+					/>
 				<div>From: Classified</div>
 				<div>Subject: X</div>
 			</div>
--- meda-mailbox/src/app/features/NavbarFeat.tsx
+++ meda-mailbox/src/app/features/NavbarFeat.tsx
@@ -1,11 +1,21 @@
 import { ButtonComp } from "../components/ButtonComp";
+import { MUIButtonCompInterface } from "../common/interfaces/component.interface";
+
+const muiButtonProps: MUIButtonCompInterface = {
+	size: 'large',
+	color: 'primary',
+	variant: 'contained',
+}
 
 export function NavbarFeat() {
 	return (
 		<div>
 			<p> Hi V. </p>
 			<p> {`You have X unread messages out of Y`} </p>
-			<ButtonComp text="View Messages" />
+			<ButtonComp 
+				text="View Messages"
+				mui={ muiButtonProps }
+				/>
 		</div>
 	);
 }
