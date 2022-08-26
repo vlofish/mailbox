99f9088609db6ad759a0368cae996deb90b1b9eb
vmijael <1945.github.m0dtb@slmail.me>
2022-08-25 20:58:42 -0500

    #10 [Navbar Feat Updated]

--- meda-mailbox/src/app/features/NavbarFeat.tsx
+++ meda-mailbox/src/app/features/NavbarFeat.tsx
@@ -1,41 +1,19 @@
-// =========================================================================
-import { Dispatch } from "redux";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { ButtonComp } from "../components/ButtonComp";
-import { MailboxInterface } from "../common/interfaces/";
-import { MUIButtonCompInterface } from "../common/interfaces/component.interface";
-import { fetchAllMessagesThunk } from "../common/store/thunks/mailbox.thunk";
-// =========================================================================
-
-let total: number;
-let unread: number;
-let dispatch: Dispatch<any>;
-
-const muiButtonProps: MUIButtonCompInterface = {
-	size: 'large',
-	color: 'primary',
-	variant: 'contained',
-}
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { MainGreetingComp } from '../components/MainGreetingComp';
 
 export function NavbarFeat() {
-	dispatch = useDispatch();
-	total = useSelector((state: MailboxInterface) => state.total);
-	unread = useSelector((state: MailboxInterface) => state.unread);
-
-	useEffect(() => {
-		dispatch(fetchAllMessagesThunk());
-	}, [])
-
 	return (
-		<div>
-			<p> Hi V. </p>
-			<p> {`You have ${unread} unread messages out of ${total} messages.`} </p>
-			<ButtonComp 
-				text="View Messages"
-				mui={ muiButtonProps }
-				/>
-		</div>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="subtitle1" component="div" sx={{ flexGrow: 1 }}>
+						<MainGreetingComp />
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 }
