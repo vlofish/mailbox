2dc3ec64d67aace260b6a4bff84d02eb6f2d57fd
vmijael <1945.github.m0dtb@slmail.me>
2022-08-25 20:58:20 -0500

    #10 [MainGreeting Comp Created]

---
+++ meda-mailbox/src/app/components/MainGreetingComp.tsx
@@ -0,0 +1,36 @@
+// =========================================================================
+import { Dispatch } from "redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { ButtonComp } from "../components/ButtonComp";
+import { MailboxInterface } from "../common/interfaces/";
+import { fetchAllMessagesThunk } from "../common/store/thunks/mailbox.thunk";
+import { MUI_WARNING_BUTTON } from "../common/constants/button.constant";
+import { Box } from "@mui/system";
+// =========================================================================
+
+let total: number;
+let unread: number;
+let dispatch: Dispatch<any>;
+
+export function MainGreetingComp() {
+  dispatch = useDispatch();
+  total = useSelector((state: MailboxInterface) => state.total);
+  unread = useSelector((state: MailboxInterface) => state.unread);
+
+  useEffect(() => {
+    dispatch(fetchAllMessagesThunk());
+  }, [])
+
+  return (
+    <Box>
+      {/* <p> Hi Rebel </p> */}
+      <label> {`You have ${unread} unread(s) out of ${total} messages.`} </label>
+      <ButtonComp
+        text="View Messages"
+        mui={MUI_WARNING_BUTTON}
+      />
+    </Box>
+  );
+}
