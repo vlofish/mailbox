cfd037266ec820550c72f2a60d005a60a18358bf
vmijael <1945.github.m0dtb@slmail.me>
2022-08-28 10:33:46 -0500

    #11 [Default First Draft of Routings]

--- meda-mailbox/src/App.tsx
+++ meda-mailbox/src/App.tsx
@@ -1,23 +1,30 @@
 // ======================================================================
+import { PagePathEnum } from "./app/common/enums/page-paths.enum";
+
 import { 
   Route, Link,
   BrowserRouter, Routes, 
 } from "react-router-dom";
-
 import HomePage from "./app/pages/HomePage";
 import InboxPage from "./app/pages/InboxPage";
 import SplitViewPage from "./app/pages/SplitViewPage";
 import SingleViewPage from "./app/pages/SingleViewPage";
+import MessageViewPage from "./app/pages/MessageViewPage";
 // ======================================================================
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 
+        // TODO: switch inbox and home; home displays the `view message` button.
+        // TODO: inbox displays the messages
+        */}
         <Route path='/' element={<InboxPage />} />
-        <Route path='/home' element={<HomePage />}>
-          <Route path='splitview' element={<SplitViewPage />} />
-          <Route path='singleview' element={<SingleViewPage />} />
+        <Route path={`/${PagePathEnum.INBOX}`} element={<HomePage />}>
+          <Route path={`${PagePathEnum.SPLIT_VIEW}`} element={<SplitViewPage />} />
+          <Route path={`${PagePathEnum.PANEL_VIEW}`} element={<SingleViewPage />} />
+          <Route path={`${PagePathEnum.INBOX_MESSAGE_VIEW}`} element={<MessageViewPage />} />
         </Route>
         <Route
           path="*"
