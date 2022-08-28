cefbb04601c269d998679c4529206783116fd6ff
vmijael <1945.github.m0dtb@slmail.me>
2022-08-27 20:45:41 -0500

    #11 [Adding Simple Routing]

--- meda-mailbox/src/App.tsx
+++ meda-mailbox/src/App.tsx
@@ -1,10 +1,37 @@
-import { HomePage } from "./app/pages/HomePage";
+// ======================================================================
+import { 
+  Route, Link,
+  BrowserRouter, Routes, 
+} from "react-router-dom";
+
+import HomePage from "./app/pages/HomePage";
+import InboxPage from "./app/pages/InboxPage";
+import SplitViewPage from "./app/pages/SplitViewPage";
+import SingleViewPage from "./app/pages/SingleViewPage";
+// ======================================================================
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<InboxPage />} />
+        <Route path='/home' element={<HomePage />}>
+          <Route path='splitview' element={<SplitViewPage />} />
+          <Route path='singleview' element={<SingleViewPage />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+              <Link to="/">
+                <button> Go to Inbox </button>
+              </Link>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
