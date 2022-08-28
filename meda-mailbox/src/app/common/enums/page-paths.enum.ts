bf192a55f83c0e6b547a32c1f569fe4633976e74
vmijael <1945.github.m0dtb@slmail.me>
2022-08-28 00:49:29 -0500

    #11 [Enum with Pages Routes]

---
+++ meda-mailbox/src/app/common/enums/page-paths.enum.ts
@@ -0,0 +1,10 @@
+export enum PagePathEnum {
+  INBOX = 'inbox',
+  SPLIT_VIEW = 'splitview',
+  PANEL_VIEW = 'panelview',
+  INBOX_SPLIT_VIEW = '/inbox/splitview',
+  INBOX_PANEL_VIEW = '/inbox/panelview',
+  INBOX_MESSAGE_VIEW = '/inbox/messageview',
+
+  // PANEL_VIEW = '/home/singleview', 
+}
