bc1a4475ef1c15a08954f0432661f30bd4c2d8c4
vmijael <1945.github.m0dtb@slmail.me>
2022-08-28 22:09:48 -0500

    #17 [Layout Improvements]

--- meda-mailbox/src/app/pages/HomePage.tsx
+++ meda-mailbox/src/app/pages/HomePage.tsx
@@ -1,41 +1,46 @@
 // =============================================================
 import '../common/common.css';
 
-import { Link } from 'react-router-dom';
 import { PagePathEnum } from '../common/enums/';
 import { ButtonComp } from '../components/ButtonComp';
 import { MessageCounterFeat } from '../features/MessageCounterFeat';
+import { useMailboxNavigateTo } from '../common/hooks/mailbox.hook';
 import { MUI_WARNING_BUTTON } from '../common/constants/button.constant';
 
 import Box from "@mui/material/Box/Box";
 import Grid from "@mui/material/Grid/Grid";
 // =============================================================
 
+let navigateToPath: any;
+
 const boxStyle = {
 	width: '100wh',
-	height: '100vh',
+	height: '50vh',
 	backgroundColor: 'primary.main',
-	'&:hover': {
-		backgroundColor: 'primary.dark',
-	},
 }
 
-const Inbox = () => (
-	<Grid container spacing={2} className='text-align-center'>
+const handleButtonClick = () => navigateToPath(PagePathEnum.INBOX_SPLIT_VIEW);
+
+// TODO: fix layout using mui
+const Home = () => (
+	<Grid container className='text-align-center'>
+		<Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+			<Box className='full-vector-height-width' sx={boxStyle} />
+		</Grid>
 		<Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
 			<Box className='full-vector-height-width' sx={boxStyle}>
+				<ButtonComp
+					text="View Messages"
+					mui={MUI_WARNING_BUTTON}
+					handleClick={handleButtonClick}
+				/>
 				<MessageCounterFeat />
-				<Link to={PagePathEnum.INBOX_SPLIT_VIEW}>
-					<ButtonComp
-						text="View Messages"
-						mui={MUI_WARNING_BUTTON}
-					/>
-				</Link>
 			</Box>
 		</Grid>
 	</Grid>
 )
 
 export default function HomePage() {
-	return <Inbox />
+	[, navigateToPath] = useMailboxNavigateTo();
+	return <Home />
 }
--- meda-mailbox/src/app/pages/InboxPage.tsx
+++ meda-mailbox/src/app/pages/InboxPage.tsx
@@ -4,13 +4,22 @@ import { FeedbackFeat } from '../features/FeedbackFeat';
 
 import Container from '@mui/material/Container';
 import AppBarPage from './AppBarPage';
+import Grid from '@mui/material/Grid/Grid';
 // =============================================================
 
 const Home = () => (
   <Container maxWidth="md">
-    <AppBarPage />
-    <Outlet />
-    <FeedbackFeat />
+    <Grid container rowSpacing={1}>
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+        <AppBarPage />
+      </Grid>
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+        <Outlet />
+      </Grid>
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className='text-align-right'>
+        <FeedbackFeat />
+      </Grid>
+    </Grid>
   </Container>
 )
 
